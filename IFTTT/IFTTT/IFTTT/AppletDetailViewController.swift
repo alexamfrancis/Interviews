b96a71feb979a03ee73d1b8f1d30e47482af0f06
afrancis <alexafrancis@apple.com>
2019-09-25 03:33:30 -0700

    fix the constraints centering issues

--- IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
+++ IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class AppletDetailViewController: UIViewController {
     var applet: Applet
     
+    private lazy var cancelButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissView))
+    }()
+
     init(_ applet: Applet) {
         self.applet = applet
         super.init(nibName: nil, bundle: nil)
@@ -22,11 +26,31 @@ class AppletDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        self.view.clipsToBounds = true
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraints()
+        self.view.layer.cornerRadius = UIScreen.main.bounds.width * 1.2
+        self.view.backgroundColor = self.applet.status == .enabled ? .orangeAppletBackgroundColor
+            : .disabledAppletBackgroundColor
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.dismissView))
         swipeDown.direction = UISwipeGestureRecognizer.Direction.down
         self.view.addGestureRecognizer(swipeDown)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.dismissView))
+//        self.navigationController?.navigationBar.setItems([UINavigationItem(title: "DONE BITCH")], animated: true)
+//            self.navigationController?.navigationBar.setItems([UIBarButtonItem(title: "DONE BITCH", style: .done, target: self, action: #selector(self.dismissView))], animated: true)
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.dismissView))
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.dismissView))
+        
+//        self.navigationController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.dismissView))
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            self.view.centerXAnchor.constraint(equalTo: self.view.superview?.centerXAnchor),
+            self.view.centerYAnchor.constraint(equalTo: self.view.superview?.centerYAnchor),
+            self.view.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            self.view.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)
+            ])
     }
     
     @objc private func dismissView() {
