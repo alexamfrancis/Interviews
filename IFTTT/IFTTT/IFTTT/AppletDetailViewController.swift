715f0da0ad46cb4aedc5daf2afc255764989738d
afrancis <alexafrancis@apple.com>
2019-09-27 00:37:10 -0700

    centered but no labels

--- IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
+++ IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
@@ -9,8 +9,75 @@
 import UIKit
 
 class AppletDetailViewController: UIViewController {
-    var applet: Applet
+    private struct Constants {
+        /// Margin from right anchor of safe area to right anchor of Image
+        static let collectionViewVerticalInset: CGFloat = 2.0
+        static let standardSpacing: CGFloat = 8.0
+        static let standardMargin: CGFloat = 32.0
+        static let iconDimensions: CGFloat = 44.0
+    }
     
+    var applet: Applet
+
+    var nameLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    var authorLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    var idLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+
+    var descriptionLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    
+    var labelStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.clipsToBounds = true
+        stackView.backgroundColor = .clear
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    var channelStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.clipsToBounds = true
+        stackView.backgroundColor = .clear
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+
     private lazy var doneButtonItem: UIBarButtonItem = {
         return UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissView))
     }()
@@ -63,6 +130,27 @@ class AppletDetailViewController: UIViewController {
             ])
         self.view.bounds = CGRect(x: navigationView.frame.width - self.diameter, y: navigationView.frame.minY + self.diameter, width: self.diameter, height: self.diameter)
         self.viewDidLayoutSubviews()
+    }
+    
+    private func configureLabels() {
+        guard let channels = self.applet.channels else { return }
+        for channel in channels {
+            let image = UIImage(contentsOfFile: channel.image_url)
+            self.channelStackView.addArrangedSubview(UIImageView(image: image))
+        }
+        self.nameLabel.text = self.applet.name
+        self.idLabel.text = self.applet.id
+        self.authorLabel.text = self.applet.author
+        self.idLabel.text = self.applet.id
+        self.descriptionLabel.text = self.applet.description
+        
+        self.labelStackView.addArrangedSubview(self.nameLabel)
+        self.labelStackView.addArrangedSubview(self.idLabel)
+        self.labelStackView.addArrangedSubview(self.authorLabel)
+        self.labelStackView.addArrangedSubview(self.channelStackView)
+        self.labelStackView.addArrangedSubview(self.descriptionLabel)
+        self.view.addSubview(self.labelStackView)
+        
     }
     
     @objc private func dismissView() {
