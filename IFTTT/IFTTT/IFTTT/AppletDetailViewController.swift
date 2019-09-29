93232c277d5e3e531b922ddbdbd731064f349520
afrancis <alexafrancis@apple.com>
2019-09-29 16:33:46 -0700

    going to scrap the view controller and start again

--- IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
+++ IFTTT/IFTTT/IFTTT/AppletDetailViewController.swift
@@ -8,58 +8,43 @@
 
 import UIKit
 
-class AppletDetailViewController: UIViewController {
-    private struct Constants {
-        /// Margin from right anchor of safe area to right anchor of Image
-        static let collectionViewVerticalInset: CGFloat = 2.0
-        static let standardSpacing: CGFloat = 8.0
-        static let standardMargin: CGFloat = 32.0
-        static let iconDimensions: CGFloat = 44.0
+class Label: UILabel {
+    init(rect: CGRect) {
+        super.init(frame: .zero)
+        self.textColor = .white
+        self.autoresizesSubviews = true
+        self.numberOfLines = 0
+        self.lineBreakMode = .byWordWrapping
+        self.font = UIFont.preferredFont(forTextStyle: .body)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class AppletDetailViewController: UIViewController {
     var applet: Applet
 
-    lazy var nameLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.textColor = .white
-        label.numberOfLines = 0
-        label.font = UIFont.preferredFont(forTextStyle: .headline)
-        label.lineBreakMode = .byWordWrapping
-        return label
+    var nameLabel: Label = {
+        return Label(rect: .zero)
     }()
     
-    lazy var authorLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.textColor = .white
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        return label
+    var authorLabel: Label = {
+        return Label(rect: .zero)
     }()
     
-    lazy var idLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.textColor = .white
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        return label
+    var idLabel: Label = {
+        return Label(rect: .zero)
     }()
 
-    lazy var descriptionLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.textColor = .white
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        return label
+    var descriptionLabel: Label = {
+        return Label(rect: .zero)
     }()
     
     lazy var labelStackView: UIStackView = {
-        let stackView = UIStackView(frame: self.view.frame)
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.clipsToBounds = true
-        stackView.backgroundColor = .clear
+        let stackView = UIStackView(frame: .zero)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .leading
         stackView.distribution = .equalSpacing
@@ -69,7 +54,7 @@ class AppletDetailViewController: UIViewController {
     lazy var channelStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
 //        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.clipsToBounds = true
+//        stackView.clipsToBounds = true
         stackView.backgroundColor = .clear
         stackView.axis = .horizontal
         stackView.alignment = .leading
@@ -96,36 +81,39 @@ class AppletDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let navigationView = self.navigationController?.view else {
-            print("no navigation view controller in the applet detail screen")
-            return
-        }
-        self.view.frame = self.getViewFrame()
-        self.view.bounds = self.getViewFrame()
-        self.view.clipsToBounds = true
-        self.view.layer.masksToBounds = true
-        self.view.layer.cornerRadius = self.diameter / 2
-        self.view.backgroundColor = self.applet.status == .enabled ? .orangeAppletBackgroundColor
-            : .disabledAppletBackgroundColor
+//        self.viewWillLayoutSubviews()
+//        guard let navigationView = self.navigationController?.view else {
+//            print("no navigation view controller in the applet detail screen")
+//            return
+//        }
+//        self.view.clipsToBounds = true
+//        self.view.frame = self.getViewFrame()
+//        self.view.bounds = self.getViewFrame()
+//        self.viewDidLayoutSubviews()
+//        self.view.layer.masksToBounds = true
+//        self.view.layer.cornerRadius = self.diameter / 2
+//        self.view.backgroundColor = self.applet.status == .enabled ? .orangeAppletBackgroundColor
+//            : .disabledAppletBackgroundColor
+//        self.view.reloadInputViews()
 //        self.view.clipsToBounds = true
 //        self.view.layer.cornerRadius = self.diameter / 2
 //        self.addConstraints()
-//        self.configureLabels()
+        self.configureLabels()
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.dismissView))
         swipeDown.direction = UISwipeGestureRecognizer.Direction.down
         self.view.addGestureRecognizer(swipeDown)
         self.navigationItem.rightBarButtonItem = self.doneButtonItem
-//        self.viewWillLayoutSubviews()
 //        self.addConstraints()
-        self.configureLabels()
+//        self.configureLabels()
 //        self.viewDidLayoutSubviews()
+//        self.view.reloadInputViews()
 }
     
-    private func getViewFrame() -> CGRect {
-        let x = (UIScreen.main.bounds.width - self.diameter) / 2
-        let y = (UIScreen.main.bounds.height - self.diameter) / 2
-        return CGRect(x: x, y: y, width: diameter, height: diameter)
-    }
+//    private func getViewFrame() -> CGRect {
+//        let x = (UIScreen.main.bounds.width - self.diameter) / 2
+//        let y = (UIScreen.main.bounds.height - self.diameter) / 2
+//        return CGRect(x: x, y: y, width: diameter, height: diameter)
+//    }
 
 //    override func viewDidAppear(_ animated: Bool) {
 //        super.viewDidAppear(animated)
@@ -144,41 +132,69 @@ class AppletDetailViewController: UIViewController {
 //        self.view.layer.cornerRadius = self.diameter / 2
 //    }
     
-    private func addConstraints() {
+//    private func addConstraints() {
 //        self.view.removeConstraints(self.view.constraints)
-        guard let navigationView = self.navigationController?.view else {
-            print("no navigation view controller in the applet detail screen")
-            return
-        }
-        NSLayoutConstraint.activate([
-            self.view.widthAnchor.constraint(equalToConstant: self.diameter),
-            self.view.heightAnchor.constraint(equalToConstant: self.diameter)
-            ])
-        self.view.frame = CGRect(x: navigationView.frame.width - self.diameter, y: navigationView.frame.minY + self.diameter, width: self.diameter, height: self.diameter)
+//        guard let navigationView = self.navigationController?.view else {
+//            print("no navigation view controller in the applet detail screen")
+//            return
+//        }
+//        NSLayoutConstraint.activate([
+//            self.view.widthAnchor.constraint(equalToConstant: self.diameter),
+//            self.view.heightAnchor.constraint(equalToConstant: self.diameter)
+//            ])
+//        self.view.frame = CGRect(x: navigationView.frame.width - self.diameter, y: navigationView.frame.minY + self.diameter, width: self.diameter, height: self.diameter)
 //        self.viewDidLayoutSubviews()
-    }
+//    }
     
     private func configureLabels() {
-        guard let channels = self.applet.channels else { return }
-        for channel in channels {
-            let image = UIImage(contentsOfFile: channel.image_url)
-            self.channelStackView.addSubview(UIImageView(image: image))
-        }
         self.nameLabel.text = self.applet.name
         self.idLabel.text = self.applet.id
         self.authorLabel.text = self.applet.author
         self.idLabel.text = self.applet.id
         self.descriptionLabel.text = self.applet.description
-        self.labelStackView.frame = self.view.frame
         
-        self.labelStackView.addSubview(self.nameLabel)
-        self.labelStackView.addSubview(self.idLabel)
-        self.labelStackView.addSubview(self.authorLabel)
-        self.labelStackView.addSubview(self.channelStackView)
-        self.labelStackView.addSubview(self.descriptionLabel)
+        
+        self.labelStackView.addArrangedSubview(self.nameLabel)
+        self.labelStackView.addArrangedSubview(self.idLabel)
+        self.labelStackView.addArrangedSubview(self.authorLabel)
+//        guard let channels = self.applet.channels else { return }
+//        for channel in channels {
+//            let image = UIImage(contentsOfFile: channel.image_url)
+//            self.channelStackView.addSubview(UIImageView(image: image))
+//        }
+//        self.labelStackView.addSubview(self.channelStackView)
+        self.labelStackView.addArrangedSubview(self.descriptionLabel)
+        
+        let button = UIButton(type: .roundedRect)
+        button.setTitle("ALEXA FRANCIS", for: .normal)
+        self.labelStackView.contentMode = .center
+        self.labelStackView.addArrangedSubview(button)
+
+        self.labelStackView.clipsToBounds = true
+        self.labelStackView.layer.masksToBounds = true
+        self.labelStackView.layer.cornerRadius = self.diameter / 2
+        self.labelStackView.layer.backgroundColor = self.applet.status ?? .disabled == .enabled ? UIColor.orangeAppletBackgroundColor.cgColor : UIColor.disabledAppletBackgroundColor.cgColor
+        self.labelStackView.backgroundColor = self.applet.status == .enabled ? .orangeAppletBackgroundColor : .disabledAppletBackgroundColor
+
         self.view.addSubview(self.labelStackView)
-//
+        self.addSubviewConstraints()
+
     }
+
+    private func addSubviewConstraints() {
+        NSLayoutConstraint.activate([
+            self.labelStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.labelStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.labelStackView.widthAnchor.constraint(equalToConstant: self.diameter),
+            self.labelStackView.heightAnchor.constraint(equalToConstant: self.diameter)
+            ])
+//        self.labelStackView.clipsToBounds = true
+//        self.labelStackView.layer.masksToBounds = true
+//        self.labelStackView.layer.cornerRadius = self.diameter / 2
+//        self.view.setNeedsLayout()
+//        self.view.layoutIfNeeded()
+    }
+
     
     @objc private func dismissView() {
         self.dismiss(animated: true, completion: nil)
